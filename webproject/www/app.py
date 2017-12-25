16d7e5c02166a057d7924648c0b04afdb4fb3601
rd0569 <m18290107861@163.com>
2017-12-25 18:01:27 +0800

    测试model成功

--- webproject/www/app.py
+++ webproject/www/app.py
@@ -19,20 +19,20 @@ def index(request):
 	return web.Response(body='<h1>WangCY</h1>')
 
 async def init(loop):
-	# await orm.create_pool(loop=loop, user='root', password='root', db='test')
+	await orm.create_pool(loop=loop, user='root', password='root', db='test')
 	# user = User(name='wangchunyun', email='624332772@qq.com', passwd='123456', admin=True, image='/2017/12/15/4546rr.jpg')
-	# user = User(name='yunyun', email='14334332772@qq.com', created_at=1514189784.19922, passwd='7888888', admin=True, image='/2017/11/15/4ghghfhrr.jpg',id='0015141897841980b0a0929420c47a89ab4283f71aac817000')
+	user = User(name='yunyun', email='14334332772@qq.com', created_at=1514189784.19922, passwd='7888888', admin=True, image='/2017/11/15/4ghghfhrr.jpg',id='0015141897841980b0a0929420c47a89ab4283f71aac817000')
 	# user = User(id='0015141897841980b0a0929420c47a89ab4283f71aac817000')
 	# user = User()
 	# print(await user.find('0015141897841980b0a0929420c47a89ab4283f71aac817000'))
 	# print(await user.update())
-	# print(await user.remove())
+	print(await user.remove())
 
-	app = web.Application(loop=loop)
-	app.router.add_route('GET','/',index)
-	srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
-	logging.info('server started at http://127.0.0.1:9000')
-	return srv
+	# app = web.Application(loop=loop)
+	# app.router.add_route('GET','/',index)
+	# srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+	# logging.info('server started at http://127.0.0.1:9000')
+	# return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
--- webproject/www/orm.py
+++ webproject/www/orm.py
@@ -8,8 +8,8 @@ import asyncio,logging
 import aiomysql
 
 
-def log(sql,args=()):
-	logging.info('SQL: %s,ARGS: %s' % (sql,args))
+def log(sql,args):
+	logging.info('SQL: %s ===> ARGS: %s' % (sql,args))
 
 async def create_pool(loop,**kw):
 	logging.info('create database connection pool....')
