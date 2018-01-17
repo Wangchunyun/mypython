bfb62f9e33cfb86571e14ae14e6ee73cebbca2f7
rd0569 <m18290107861@163.com>
2018-01-17 16:58:33 +0800

    完成配置文件编写

---
+++ webproject/www/config_default.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+default config
+'''
+
+__author__ = 'WangCY'
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'root',
+        'password':'root',
+        'db':'test',
+    },
+    'session':{
+        'secret':'Root'
+    }
+}
+
---
+++ webproject/www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override config
+'''
+
+__author__ = 'WangCY'
+
+
+configs = {
+    'db':{
+        'host':'127.0.0.1'
+    }
+}
